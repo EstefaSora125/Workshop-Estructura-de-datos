9be6f459f0b37016f38ae35cb107fcd1c0276728
Estefanía <estefayiso@gmail.com>
2024-05-27 18:30:34 -0500

    Ajuste HTML Validator

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,16 +1,30 @@
 import java.util.Queue;
 import java.util.Stack;
 
-
-public class HtmlValidator {
+public class HtmlValidator{
 
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+		Stack<HtmlTag> newStack = new Stack<>();
+		double tagsSize = tags.size();
 
-		/* IMPLEMENT THIS METHOD! */
-
-		return null; // this line is here only so this code will compile if you don't modify it
+		for (int i = 0; i < tagsSize; i++) {
+			if (tags.element().isSelfClosing()) {
+				tags.remove();
+			} else if (tags.element().isOpenTag()) {
+				newStack.add(tags.element());
+				tags.remove();
+			}else if (!newStack.isEmpty()){
+				if (tags.element().matches(newStack.peek())) {
+					newStack.pop();
+					tags.remove();
+				}
+			}
+			else {
+				if (!tags.isEmpty() && !tags.element().isOpenTag()){
+					return null;
+				}
+			}
+		}
+		return newStack;
 	}
-	
-
 }
-
